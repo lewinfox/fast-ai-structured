ad3e35471c91f7e479bbb6325a1e3f529fe47bf9
Lewin Appleton-Fox <lewin.a.f@gmail.com>
2019-04-01 23:04:09 +0100

    Add detect_outliers

---
+++ R/detect_outliers.R
@@ -0,0 +1,51 @@
+#' Flag outliers
+#'
+#' Flag any values that fall outside a specified range. By default, any
+#' observation more than 1.5x the interquartile range will be tagged. The `clip`
+#' option controls whether values outside this range will be trimmed to the
+#' appropriate max or min.
+#'
+#' @param df The data.frame to be transformed
+#' @param col The column of interest
+#' @param threshold Values more than \code{threshold} times the interquartile
+#'   range outside the interquartile range are considered outliers.
+#' @param clip If \code{TRUE}, values above or below the specified threshold
+#'   will be clipped to the relevant threshold.
+#' @param flag_both_directions If \code{TRUE}, two boolean columns will be added
+#'   to indicate whether the values was above or below the detection threshold.
+#' @export
+detect_outliers <- function(df, col, threshold = 1.5, clip = FALSE,
+                            flag_both_directions = FALSE) {
+  # Input checking
+  stopifnot(is.data.frame(df))
+  if (!is.numeric(df[[col]])) {
+    warning(col, " is not a numeric column - ignoring")
+    return(df)
+  }
+  # Calculate limits
+  q_25 <- quantile(df[[col]], 0.25)
+  q_75 <- quantile(df[[col]], 0.75)
+  iqr <- IQR(df[[col]])
+  threshold_lower <- q_25 - (threshold * iqr)
+  threshold_upper <- q_75 + (threshold * iqr)
+  # Identify outliers
+  is_outlier_lower <- df[[col]] < threshold_lower
+  is_outlier_upper <- df[[col]] > threshold_upper
+  is_outlier <- is_outlier_lower | is_outlier_upper
+  # Clip values if requested
+  if (clip) {
+    df[[col]][is_outlier_lower] <- threshold_lower
+    df[[col]][is_outlier_upper] <- threshold_upper
+  }
+  # Add boolean indicators to data.frame
+  if (flag_both_directions) {
+    flag_col_upper <- paste(col, "outlier_upper", sep = "_")
+    flag_col_lower <- paste(col, "outlier_lower", sep = "_")
+    df[[flag_col_upper]] <- is_outlier_upper
+    df[[flag_col_lower]] <- is_outlier_lower
+  } else {
+    new_col_name <- paste(col, "outlier", sep = "_")
+    df[[new_col_name]] <- is_outlier
+  }
+  return(df)
+}
---
+++ tests/testthat/test_detect_outliers.R
@@ -0,0 +1,32 @@
+context("Outlier detection")
+
+df <- iris
+
+test_that("internal params are calculated correctly", {
+  # These are just the commands that are executed inside the function
+  iqr <- IQR(df$Sepal.Width)
+  q_25 <- quantile(df$Sepal.Width, 0.25)
+  q_75 <- quantile(df$Sepal.Width, 0.75)
+  threshold_lower <- q_25 - (1.5 * iqr)
+  threshold_upper <- q_75 + (1.5 * iqr)
+  # Test them against the actual outcome
+  clipped_df <- detect_outliers(df, "Sepal.Width", threshold = 1.5, clip = TRUE)
+  expect_false(max(clipped_df$Sepal.Width) > threshold_upper)
+  expect_false(min(clipped_df$Sepal.Width) < threshold_lower)
+  expect_equal(min(clipped_df$Sepal.Width), unname(threshold_lower))
+  expect_equal(max(clipped_df$Sepal.Width), unname(threshold_upper))
+})
+
+test_that("clipping works correctly", {
+  clipped_df <- detect_outliers(df, "Sepal.Width", clip = TRUE)
+  expect_false(max(df[["Sepal.Width"]]) == max(clipped_df[["Sepal.Width"]]))
+  expect_false(min(df[["Sepal.Width"]]) == min(clipped_df[["Sepal.Width"]]))
+  expect_equal(sum(clipped_df$Sepal.Width_outlier), 4)
+})
+
+test_that("bidirectional outlier flag works", {
+  clipped_df <- detect_outliers(df, "Sepal.Length", clip = TRUE, flag_both_directions = T)
+  expect_equal(colnames(clipped_df),
+               c("Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width",
+                 "Species", "Sepal.Length_outlier_upper", "Sepal.Length_outlier_lower"))
+})
